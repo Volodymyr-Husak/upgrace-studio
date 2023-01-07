cb897fe841c4e25d69ce64a982f882ad93d1f994
Volodymyr Husak <volodyaog2907@gmail.com>
2023-01-07 13:02:24 +0200

    Creates AppBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+import { Layout } from './Layout';
+const MainPage = lazy(() => import('../pages/Main'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        {/* <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        /> */}
+      </Route>
+    </Routes>
   );
 };
